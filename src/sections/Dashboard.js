02dba7e533237fe1042e43ff602412cc739e4f0e
AubreyHeartA <arian.aubreyheart18@gmail.com>
2024-05-23 06:27:16 +0800

    ....

--- src/sections/Dashboard.js
+++ src/sections/Dashboard.js
@@ -55,7 +55,7 @@ export default function Dashboard({ ongoingTasks, completedTasks, taskDetails })
                         </div>
                     ))
                 )} */}
-            </div>
+            {/* </div> */}
         </div>
     );
 }

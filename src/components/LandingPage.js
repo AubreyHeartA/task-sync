30e4ba3e22614f80ecafd3d83110337ca38c0af7
AubreyHeartA <arian.aubreyheart18@gmail.com>
2024-05-22 08:56:46 +0800

    ...

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../config/style.css';
-import TaskSync from '../assets/TaskSync1.png';  // Assuming you have this image in your src folder
+import photo from '../assets/123.jpg';  // Assuming you have this image in your src folder
 
 function LandingPage({ onSwitchToLogin, onSwitchToSignup }) {
     return (
@@ -10,7 +10,6 @@ function LandingPage({ onSwitchToLogin, onSwitchToSignup }) {
                     <h2>TaskSync</h2>
                 </div>
                 <ul className="nav-links">
-                    <li><a href="#about">About Us</a></li>
                     <li><a href="#login" onClick={onSwitchToLogin}>Log In</a></li>
                     <li><a href="#get-started" className="get-started-btn" onClick={onSwitchToSignup}>Get Started</a></li>
                     <li><a href="#get-app" className="get-app-btn">Get App</a></li>
@@ -19,11 +18,12 @@ function LandingPage({ onSwitchToLogin, onSwitchToSignup }) {
             <header className="App-header">
                 <div className="header-content">
                     <h1>Collaboration and Organization</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <p>Unlock the power of collaboration and organization with our platform, designed to 
+                    seamlessly bring teams together, streamline workflows, and achieve extraordinary results</p>
                     <button className="get-started-button" onClick={onSwitchToSignup}>Get Started</button>
                 </div>
                 <div className="header-image">
-                    <img src={TaskSync} alt="Calendar and Hourglass" />
+                    <img className='photo' src={photo} alt="Calendar and Hourglass" />
                 </div>
             </header>
         </div>
